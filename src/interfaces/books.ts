e5be4d5d234a217bb20081ffa11be1efffd65948
Gaston Ezequiel Campos <gaston.campos@santexgroup.com>
2022-07-19 10:41:03 -0300

    created interface for books

---
+++ src/interfaces/books.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    isbn: string;
+    title: string;
+    subtitle: string;
+    author: string;
+    published: string;
+    image: string;
+    description: string;
+    genres: string;
+}
